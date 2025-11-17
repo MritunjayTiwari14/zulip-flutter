61fbff2e0415e06fc5c930f0ae278531eb45f791
Mritunjay Tiwari <shreyashtiwari14.11@gmail.com>
2025-11-17 19:07:43 +0530

    Revert "dialog: Start-align dialog content on iOS, instead of center-align"
    This reverts commit d0f4de4ea1ccc152d72227134fbb17d77539df47.

--- lib/widgets/dialog.dart
+++ lib/widgets/dialog.dart
@@ -63,14 +63,7 @@ Widget? _adaptiveContent(Widget? content) {
     case TargetPlatform.macOS:
       // A [SingleChildScrollView] (wrapping both title and content) is already
       // created by [CupertinoAlertDialog].
-      return DefaultTextStyle.merge(
-        // The "alert description" is start-aligned in one example in Apple's
-        // HIG document:
-        //   https://developer.apple.com/design/human-interface-guidelines/alerts#Anatomy
-        // (Confusingly, in 2025-10, it's center-aligned in the graphic at the
-        // *top* of that page; shrug.)
-        textAlign: TextAlign.start,
-        child: content);
+      return content;
   }
 }
 
